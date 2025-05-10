d2b4c630c39f8175c1421cb8ecaf2e3351e22f02
David Tennander <david.tennander@gmail.com>
2025-05-10 11:23:14 +0200

    Clean it up

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use std::marker::PhantomData;
-
 use beavy_game::cursor::{CursorCoords, CursorPlugin};
 use bevy::{
     dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
     prelude::*,
     text::FontSmoothing,
 };
-
-struct Matrix {
-    columns: usize,
-    rows: usize,
-}
 
 /// Used to help identify our main camera
 #[derive(Component)]
@@ -22,48 +15,42 @@ struct Tile {
     y: f32,
 }
 
-fn start_up(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+#[derive(Component)]
+struct DefaultColor(Color);
+
+fn start_up(mut commands: Commands) {
     commands.spawn((MainCamera, Camera2d));
     let columns = 20;
     let rows = 20;
-    let mut tiles = vec![];
-    for _ in 0..rows {
-        let mut row = vec![];
-        for _ in 0..columns {
-            row.push(meshes.add(Rectangle::new(20., 20.)));
-        }
-        tiles.push(row);
-    }
 
-    for (i, row) in tiles.into_iter().enumerate() {
-        for (j, tile) in row.into_iter().enumerate() {
+    for i in 0..rows {
+        for j in 0..columns {
             let color = Color::hsl((360 * (20 * i + j) / 420) as f32, 1.0, 0.7);
             let x = -100. + (i * 20) as f32;
             let y = -100. + (j * 20) as f32;
             commands.spawn((
                 Tile { x, y },
-                Mesh2d(tile),
-                MeshMaterial2d(materials.add(color)),
+                Sprite::from_color(color, Vec2 { x: 20., y: 20. }),
+                DefaultColor(color),
                 Transform::from_xyz(x, y, 0.),
             ));
         }
     }
 }
 
-fn mouse_motion(
-    tiles: Query<(&mut MeshMaterial2d<ColorMaterial>, &Tile)>,
-    cursor: Res<CursorCoords>,
-) {
-    if let Some((color, _)) = tiles.iter().find(|(_, tile)| {
-        let in_x = tile.x <= cursor.0.x && cursor.0.x <= tile.x + 20.;
-        let in_y = tile.y <= cursor.0.y && cursor.0.y <= tile.y + 20.;
-        in_x && in_y
-    }) {
-        dbg!(color);
+fn mouse_motion(mut tiles: Query<(&mut Sprite, &DefaultColor, &Tile)>, cursor: Res<CursorCoords>) {
+    for (mut sprite, DefaultColor(color), tile) in &mut tiles {
+        let Vec2 {
+            x: width,
+            y: height,
+        } = sprite.custom_size.expect("We know it has a color!");
+        let in_x = tile.x - (width / 2.) <= cursor.0.x && cursor.0.x <= tile.x + width / 2.;
+        let in_y = tile.y - (height / 2.) <= cursor.0.y && cursor.0.y <= tile.y + height / 2.;
+        if in_x && in_y {
+            sprite.color = color.darker(0.2);
+        } else {
+            sprite.color = *color;
+        }
     }
 }
 
